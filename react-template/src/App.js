559f9887da2078f5a45edc703f13cf53f8dca2ef
Nomuun <na_no2iher@yahoo.com>
2021-01-05 00:16:26 +0800

    react-template added 2

---
+++ react-template/src/App.js
@@ -0,0 +1,10 @@
+import {HeaderTop} from './HeaderTop.js'
+import {HeaderLarge} from './HeaderLarge.js'
+
+export function App(props){
+    return (
+        <div>
+            app
+        </div>        
+    )
+}
